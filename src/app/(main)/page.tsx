2f8861e45d1da14e1fdaa0e7f4ad71a9d483f2e9
Munadir <munadir040325@gmail.com>
2025-11-14 09:22:39 +0000

    layout utama jadi yang rusak bro. halaman dialog cetak dan new tab sudah

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,6 +1,5 @@
 
 
-
 'use client';
 
 import { useState, useEffect, useCallback, useMemo, Fragment } from 'react';
@@ -1024,3 +1023,4 @@ export default function CalendarPage() {
     </div>
   );
 }
+
