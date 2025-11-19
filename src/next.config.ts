1d5dfaa47a4529ad08c0217f2821de9ba545ad6f
Munadir <munadir040325@gmail.com>
2025-11-19 22:16:41 +0000

    The app isn't starting. Please investigate what could be wrong based on

---
+++ src/next.config.ts
@@ -0,0 +1,45 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https'
+        ,
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  allowedDevOrigins: [
+      "https://9000-firebase-studio-1761494939100.cluster-ancjwrkgr5dvux4qug5rbzyc2y.cloudworkstations.dev"
+  ]
+};
+
+export default nextConfig;
